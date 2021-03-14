9f04a8d0147e43c8ddabb15ba261f4295f1f4bbf
Ajay Srinivas <ajaysrinivas530@gmail.com>
2021-03-14 11:36:07 +0530

    Refactor folder structure

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
         "<rootDir>/src"
     ],
     "testMatch": [
-        "**/__tests__/**/*.+(ts|tsx|js)",
+        "**/tests/**/*.+(ts|tsx|js)",
         "**/?(*.)+(spec|test).+(ts|tsx|js)"
     ],
     "transform": {
